7bf69e6011be7f561b8c70822accde214e11cf74
YowKeanYung <103058706+YowKeanYung@users.noreply.github.com>
2022-04-07 18:33:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "assignment.cegsftmiit0a.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "yowkeanyung-assignnment"
+customregion = "us-east-1"
